cb14a644b4bae1eb4b58b7df0c6e7937ebb41546
prakash-urkude <115548645+prakash-urkude@users.noreply.github.com>
2023-01-02 01:27:16 +0530

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const  mongoose  = require('mongoose')
+
+const route = require('./routes/route')
+
+
+app.use(express.json())
+mongoose.set('strictQuery', true)
+
+mongoose.connect('mongodb+srv://prakashurkude:prakash1998@cluster0.nuhssqs.mongodb.net/vaccinationApp', { useNewUrlParser : true })
+.then( () => console.log("MongoDB is connected"))
+.catch( (err) => console.log(err.message))
+
+app.use('/', route)
+
+app.listen(3000, function(){
+    console.log("Express app is running on port 3000")
+})
